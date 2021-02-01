404441ec10a713ec3d9672bede0e2b3cbdc1dd88
Jaime Guaman <jaime@guaman.cl>
2021-01-31 22:48:11 -0300

    welcome to zombocom

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import { useEffect } from 'react';
+import ReactDOM from 'react-dom';
+
+// You don't have to use `fetch` btw, use whatever you want
+const getCounters = () => 
+  fetch('/api/v1/counter', { method: 'get' })
+    .then(res => res.json());
+
+const App = () => {
+  useEffect(() => {
+    getCounters().then(console.log, console.error);
+  }, []);
+
+  return (
+    <h1>Hello, Cornershop!</h1>
+  );
+};
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
