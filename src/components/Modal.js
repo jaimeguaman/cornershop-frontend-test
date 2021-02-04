95a8aefb05b7aee0b380f63b5d9408e5c139fec6
Jaime Guaman <jaime@guaman.cl>
2021-02-04 15:27:52 -0300

    feat(Modal): add styles and behavior tweaks

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,15 +1,24 @@
 import 'styles/ui/Modal.scss'
+import { ReactComponent as CrossIcon } from 'assets/cross-icon.svg'
 
 function Modal (props) {
+  const handleClose = () => {
+    typeof props.onModalClose === 'function' && props.onModalClose()
+  }
   return (
     <section className="app-modal">
       <div className="mask">
         <div className="modal-wrapper">
-          <div className="viewport-wrapper">
+          <div className="viewport-wrapper -flat -full-height">
             <div className="modal-container">
                 <div className="modal-header">
-                  <h2>{props.title}</h2>
-                  {props.header}
+                  <button onClick={ handleClose } className="close-modal-button">
+                    <CrossIcon/>
+                  </button>
+                  <h2 className="large-title -to-left">{props.title}</h2>
+                  <div className="header-actions">
+                    {props.header}
+                  </div>
                 </div>
                 <div className="modal-body">
                   {props.body}
