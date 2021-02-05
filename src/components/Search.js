f7996df76f9ec3cc1ff45c323e96fe4125c1786e
Jaime Guaman <jaime@guaman.cl>
2021-02-04 22:33:46 -0300

    fix(Search): add icon, behavior fix

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,6 @@
 import { useCallback, useState } from 'react'
 import 'styles/ui/Search.scss'
+import { ReactComponent as SearchIcon } from 'assets/search-icon.svg'
 
 function Search ({ onChange = () => {}, text}) {
   const [ isActive, setActive ] = useState(false)
@@ -9,13 +10,25 @@ function Search ({ onChange = () => {}, text}) {
   }, []);
 
   const handleCancel = useCallback( () => {
+    setActive(false)
     onChange('')
   }, [])
 
   return (
-    <form className={`search-box ${isActive ? "-active" : "-inactive"}`} onSubmit={e => e.preventDefault()}>
+    <form
+      className={`search-box ${isActive ? "-active" : "-inactive"}`}
+      onSubmit={e => e.preventDefault()} >
       <div className="search-box__inner">
-        <input onFocus={e => setActive(true)} onBlur={e => setActive(false)} placeholder="Search Counters" onChange={handleChange} value={text} type="text"/>
+        <i class="input-icon">
+          <SearchIcon />
+        </i>
+        <input
+          onFocus={e => setActive(true)}
+          onBlur={handleCancel}
+          placeholder="Search Counters"
+          onChange={handleChange}
+          value={text}
+          type="text" />
         <button className="standard-button" onClick={handleCancel} type="reset">Cancel</button>
       </div>
     </form>
