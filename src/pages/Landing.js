9b58f831d2a000243abdf1bf9769246c0d8ccece
Jaime Guaman <jaime@guaman.cl>
2021-02-08 07:52:02 -0300

    fix(Landing): use history to prevent full refresh

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,7 +1,9 @@
+import { useHistory } from "react-router-dom";
 import { ReactComponent as Logo } from 'assets/splash-icon.svg'
 import 'styles/pages/Landing.scss'
 
 function Landing () {
+  const history = useHistory()
   return (
     <section className="landing-page">
       <div className="viewport-wrapper -full-height">
@@ -9,7 +11,7 @@ function Landing () {
           <Logo/>
           <h2 className="large-title">Welcome to Counters</h2>
           <p className="secondary-text">Capture cups of lattes, frapuccinos, or anything else that can be counted</p>
-          <a className="accent-button cta" href="/counters/">Get started</a>
+          <button className="accent-button cta" onClick={() => history.push('/counters/')}> Get started</button>
         </div>
       </div>
     </section>
